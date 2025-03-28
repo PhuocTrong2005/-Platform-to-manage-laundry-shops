8858b62209cacd40f453ec893acd86e349296a85
tvuong17 <thanhvuong123sss@gmail.com>
2025-03-29 02:24:21 +0700

    giao dien admin system

---
+++ project/src/main/java/com/laundry/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.laundry.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController
+{
+@RequestMapping("")
+public String home() 
+{
+	return "index";
+}
+}
